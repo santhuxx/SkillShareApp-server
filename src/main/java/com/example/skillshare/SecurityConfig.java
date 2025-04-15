6c227754a2c13182493edd9ab4439f9b312c6162
Kavindu Santhusha <ksanthusha20@gmail.com>
2025-04-15 23:16:29 +0530

    Oauth configs

--- src/main/java/com/example/skillshare/SecurityConfig.java
+++ src/main/java/com/example/skillshare/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.example.skillshare;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,6 +17,13 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated())
+                //.formLogin(form -> form.defaultSuccessUrl("/hello",true))
+                .oauth2Login(oauth2 ->
+                        oauth2.defaultSuccessUrl("/hello",true));
+
 
         return http.build();
     }
