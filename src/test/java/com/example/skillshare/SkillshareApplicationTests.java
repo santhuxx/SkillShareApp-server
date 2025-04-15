acf94d8fa94738049c31b3c916560d791be2c4e3
Kavindu Santhusha <ksanthusha20@gmail.com>
2025-04-15 13:38:24 +0530

    Backend

---
+++ src/test/java/com/example/skillshare/SkillshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skillshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
